3cf589cffc975a56c70bee5098ffbfed4804f73b
“metaware” <pdw3bservices@gmail.com>
2024-06-25 15:23:28 +0530

    update

--- Hooks/MappingData.js
+++ Hooks/MappingData.js
@@ -3,8 +3,7 @@ import { MappingData } from '../GraphQl/queries'
 
 const useMapData = (enId) => {
   const { loading, error, data, refetch } = useQuery(MappingData, {
-    variables: { enId },
-    skip: !enId
+    variables: { enId }
   })
   return {
     loading,
